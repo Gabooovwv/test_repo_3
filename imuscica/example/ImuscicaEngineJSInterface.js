d8f75af6104b06378ada375405d53606e5edde78
Endre Kollath <endre.kollath@leopoly.com>
2018-08-02 13:45:07 +0200

    Engine update (guitar customization) + object updates

--- imuscica/example/ImuscicaEngineJSInterface.js
+++ imuscica/example/ImuscicaEngineJSInterface.js
@@ -4,7 +4,7 @@ var ImuscicaEngine =
     defaultMonochordData: { description: { type: "Monochord", bodyScale: 1, bridge1On: 0, bridge2On:0, bridge1Ratio: 0.3, bridge2Ratio: 0.6 } },
     defaultSquareMembraneData: { description: { type: "SquareMembrane" } },
     defaultCircleMembraneData: { description: { type: "CircleMembrane" } },
-    defaultGuitarData: { description: { type: "Guitar", chordFlags: 0 } },
+    defaultGuitarData: { description: { type: "Guitar", chord1: 0, chord2: 0, chord3: 0, chord4: 0, chord5: 0, chord6: 0 } },
     defaultXylophoneData: { description: { type: "Xylophone" } },
     
     // variables that used for return values of JS side functions (set by Unity)
