e9e33370d755427bc4a07dcf162188e0cb9510fa
Endre Kollath <endre.kollath@leopoly.com>
2018-08-28 11:17:32 +0200

    Engine update (xylophone + JS interface changes)

--- imuscica/example/ImuscicaEngineJSInterface.js
+++ imuscica/example/ImuscicaEngineJSInterface.js
@@ -2,13 +2,20 @@ var ImuscicaEngine =
 {
     // default instrument data
     defaultMonochordData: { description: { type: "Monochord", bodyScale: 1, bridge1On: 0, bridge2On:0, bridge1Ratio: 0.3, bridge2Ratio: 0.6 } },
-    defaultSquareMembraneData: { description: { type: "SquareMembrane" } },
-    defaultCircleMembraneData: { description: { type: "CircleMembrane" } },
+    defaultSquareMembraneData: { description: { type: "SquareMembrane", scalePointLength: 0.16 } },
+    defaultCircleMembraneData: { description: { type: "CircleMembrane", scalePointLength: 0.16 } },
     defaultGuitarData: { description: { type: "Guitar", chord1: 0, chord2: 0, chord3: 0, chord4: 0, chord5: 0, chord6: 0 } },
-    defaultXylophoneData: { description: { type: "Xylophone" } },
+    defaultXylophoneData: { description: { type: "Xylophone", 
+		bodyStartPos: [-0.11, 0, 0], bodyStartRot: [0, 0, 0, 1], bodyStartScale: [1, 1, 1], 
+		bodyEndPos: [0.11, 0, 0], bodyEndRot: [0, 0, 0, 1], bodyEndScale: [1, 1, 1], 
+		numBars: 5 } },
     
     // variables that used for return values of JS side functions (set by Unity)
     _isMusicStringEnabled: false,
+	_numBars: 5,
+	_barLength: 0,
+	_minBarLength: 0,
+	_maxBarLength: 0,
     _instrumentData: "",
 
     // ------------------
@@ -48,12 +55,32 @@ var ImuscicaEngine =
         return this._isMusicStringEnabled;
     },
     
-    setBarNum: function(barNum)
+    setBarNum: function(numBars)
     {
+		gameInstance.SendMessage("ImuscicaEngine", "SetBarNum", numBars);
     },
     
     getBarNum: function()
     {
-        return 0;
+        gameInstance.SendMessage("ImuscicaEngine", "QueryBarNum", index);
+        return this._numBars;
+    },
+	
+	setBarLength: function(index, length)
+    {
+		var value = index.toString() + " " + length.toString();
+		gameInstance.SendMessage("ImuscicaEngine", "SetBarLengthWithString", value);
+    },
+
+    getBarLength: function(index)
+    {
+		gameInstance.SendMessage("ImuscicaEngine", "QueryBarLength", index);
+        return this._barLength;
+    },
+
+    getBarLengthLimits: function(index)
+    {
+		gameInstance.SendMessage("ImuscicaEngine", "QueryBarLengthLimits", index);
+        return [this._minBarLength, this._maxBarLength];
     }
 };
